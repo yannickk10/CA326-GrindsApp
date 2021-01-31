acc8cfa4c018da87f2536eb031c0a63ad0c56176
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-01-31 19:35:24 +0000

    created views for student & tutor register page, also for login page

--- code/django_webapp/users/views.py
+++ code/django_webapp/users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+from .forms import CreateUserForm
+
+def student_register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'users/student_register.html', context)
+
+def tutor_register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'users/tutor_register.html', context)
+
+def login_page(request):
+    context = {}
+    return render(request, 'users/login.html', context)
