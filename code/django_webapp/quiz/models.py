b1ac2f6c50c677deca40da9c6070133e21613060
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-02-22 15:03:03 +0000

    student model

--- code/django_webapp/quiz/models.py
+++ code/django_webapp/quiz/models.py
@@ -42,3 +42,18 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.text
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    quizzes = models.ManyToManyField(Quiz, through='TakenQuiz')
+    interests = models.ManyToManyField(Subject, related_name='interested_students')
+
+    def get_unanswered_questions(self, quiz):
+        answered_questions = self.quiz_answers \
+            .filter(answer__question__quiz=quiz) \
+            .values_list('answer__question__pk', flat=True)
+        questions = quiz.questions.exclude(pk__in=answered_questions).order_by('text')
+        return questions
+
+    def __str__(self):
+        return self.user.username
