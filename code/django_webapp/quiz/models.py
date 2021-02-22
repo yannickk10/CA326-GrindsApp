81d58c085faffa0692cace812e90f81cd27a0c3c
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-02-22 15:01:27 +0000

    subject model

--- code/django_webapp/quiz/models.py
+++ code/django_webapp/quiz/models.py
@@ -7,3 +7,16 @@ from django.utils.html import escape, mark_safe
 class User(AbstractUser):
     is_student = models.BooleanField(default=False)
     is_tutor = models.BooleanField(default=False)
+
+class Subject(models.Model):
+    name = models.CharField(max_length=30)
+    color = models.CharField(max_length=7, default='#007bff')
+
+    def __str__(self):
+        return self.name
+
+    def get_html_badge(self):
+        name = escape(self.name)
+        color = escape(self.color)
+        html = '<span class="badge badge-primary" style="background-color: %s">%s</span>' % (color, name)
+        return mark_safe(html)
