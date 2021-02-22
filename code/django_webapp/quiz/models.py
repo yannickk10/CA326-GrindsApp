e86e3971218fb3cff03b296e275394b3b5adf590
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-02-22 15:00:39 +0000

    setup user model

--- code/django_webapp/quiz/models.py
+++ code/django_webapp/quiz/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.html import escape, mark_safe
+
 
 # Create your models here.
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_tutor = models.BooleanField(default=False)
