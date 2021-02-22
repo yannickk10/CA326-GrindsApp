d75fe1a51a8706d2d15ab895570e93f531df71d7
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-02-22 16:09:34 +0000

    setting up students quiz list view

---
+++ code/django_webapp/quiz/views/students.py
@@ -0,0 +1,30 @@
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.db.models import Count
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, ListView, UpdateView
+
+from ..forms import TakeQuizForm
+from ..models import Quiz, Student, TakenQuiz, User
+
+
+
+class QuizListView(ListView):
+    model = Quiz
+    ordering = ('name', )
+    context_object_name = 'quizzes'
+    template_name = 'quiz/students/quiz_list.html'
+
+    def get_queryset(self):
+        student = self.request.user.student
+        student_interests = student.interests.values_list('pk', flat=True)
+        taken_quizzes = student.quizzes.values_list('pk', flat=True)
+        queryset = Quiz.objects.filter(subject__in=student_interests) \
+            .exclude(pk__in=taken_quizzes) \
+            .annotate(questions_count=Count('questions')) \
+            .filter(questions_count__gt=0)
+        return queryset
