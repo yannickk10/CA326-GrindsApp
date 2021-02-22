1a1bd98ecdde93694e859ee7f7fc34edeb92bfac
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-02-22 15:11:30 +0000

    form for taking quiz

--- code/django_webapp/quiz/forms.py
+++ code/django_webapp/quiz/forms.py
@@ -23,3 +23,19 @@ class BaseAnswerInlineFormSet(forms.BaseInlineFormSet):
                     break
         if not has_one_correct_answer:
             raise ValidationError('Mark at least one answer as correct.', code='no_correct_answer')
+
+class TakeQuizForm(forms.ModelForm):
+    answer = forms.ModelChoiceField(
+        queryset=Answer.objects.none(),
+        widget=forms.RadioSelect(),
+        required=True,
+        empty_label=None)
+
+    class Meta:
+        model = StudentAnswer
+        fields = ('answer', )
+
+    def __init__(self, *args, **kwargs):
+        question = kwargs.pop('question')
+        super().__init__(*args, **kwargs)
+        self.fields['answer'].queryset = question.answers.order_by('text')
