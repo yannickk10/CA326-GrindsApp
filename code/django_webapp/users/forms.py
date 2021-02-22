f93144de437a8404651b247d970cf4dd2d8d99bc
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-02-22 15:50:52 +0000

    imported user model from quiz app and made tutor signup form

--- code/django_webapp/users/forms.py
+++ code/django_webapp/users/forms.py
@@ -1,13 +1,21 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from django.contrib.auth.models import User
+from django.db import transaction
+from quiz.models import User, Subject
 
 
-class CreateUserForm(UserCreationForm):
+class TutorSignUpForm(UserCreationForm):
     first_name = forms.CharField(required=True, max_length=50)
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ['first_name', 'username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_teacher = True
+        if commit:
+            user.save()
+        return user
