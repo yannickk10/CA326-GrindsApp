57e9d0f737a9e100044b229dadd36019e84ee8e6
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-02-22 15:14:36 +0000

    setup the view for the quiz list

---
+++ code/django_webapp/quiz/views/tutor.py
@@ -0,0 +1,28 @@
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.db.models import Avg, Count
+from django.forms import inlineformset_factory
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
+                                  UpdateView)
+
+from ..forms import BaseAnswerInlineFormSet, QuestionForm
+from ..models import Answer, Question, Quiz, User
+
+
+class QuizListView(ListView):
+    model = Quiz
+    ordering = ('name', )
+    context_object_name = 'quizzes'
+    template_name = 'quiz/tutors/quiz_change_list.html'
+
+    def get_queryset(self):
+        queryset = self.request.user.quizzes \
+            .select_related('subject') \
+            .annotate(questions_count=Count('questions', distinct=True)) \
+            .annotate(taken_count=Count('taken_quizzes', distinct=True))
+        return queryset
