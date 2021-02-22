27c315136f82b46e44ccc887d1e367b19268d24d
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-02-22 15:10:52 +0000

    answer structure

--- code/django_webapp/quiz/forms.py
+++ code/django_webapp/quiz/forms.py
@@ -10,3 +10,16 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ('text', )
+
+class BaseAnswerInlineFormSet(forms.BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+
+        has_one_correct_answer = False
+        for form in self.forms:
+            if not form.cleaned_data.get('DELETE', False):
+                if form.cleaned_data.get('is_correct', False):
+                    has_one_correct_answer = True
+                    break
+        if not has_one_correct_answer:
+            raise ValidationError('Mark at least one answer as correct.', code='no_correct_answer')
