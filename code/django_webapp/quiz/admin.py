1c16901bef5fa1a566bd9a65c608a7e88343dd2f
kekeref2 <fawwaz.kekereekun2@mail.dcu.ie>
2021-02-23 20:46:37 +0000

    Made models viewable on admin page

--- code/django_webapp/quiz/admin.py
+++ code/django_webapp/quiz/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Subject)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Student)
+admin.site.register(TakenQuiz)
+admin.site.register(StudentAnswer)
+admin.site.register(User)
