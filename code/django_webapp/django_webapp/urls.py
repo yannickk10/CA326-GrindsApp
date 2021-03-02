cb464d56258ce0f968d60d8ea585ed3445bcfe1c
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-03-02 11:44:44 +0000

    fixed urls

--- code/django_webapp/django_webapp/urls.py
+++ code/django_webapp/django_webapp/urls.py
@@ -16,19 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
-from dashboard import views as dashboard_view
-from quiz.views import tutor
-
+from quiz.views import tutor, students
+from dashboard.views import welcome, tutor, students
 
 
 urlpatterns = [
     path('', include('quiz.urls')),
-    path('', home_views.home_view, name='home'),
+    path('', include('dashboard.urls')),
     path('admin/', admin.site.urls),
     path('login/', user_views.login_page, name="login"),
     path('student-register/', user_views.student_register, name="student-register"),
     path('tutor-register/', user_views.tutor_register, name="tutor-register"),
-    path('contact-page/', home_views.contact_view, name="contact-page"),
-    path('tutor-dashboard/', dashboard_view.tutor_dash_view, name="tutor-dash-page"),
-    path('student-dashboard/', dashboard_view.student_dash_view, name="student-dash-page")
 ]
