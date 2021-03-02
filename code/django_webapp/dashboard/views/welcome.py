f75f1b9a5af494b18487841131bc3c06e719e5d1
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-03-02 01:02:34 +0000

    welcome views redone

---
+++ code/django_webapp/dashboard/views/welcome.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+def welcome_view(request):
+    return render(request, "dasboard/welcome.html")
+
+def contact_view(request):
+    return render(request, "dashboard/contact.html")
