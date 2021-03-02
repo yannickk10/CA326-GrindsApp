bba5a6638f33f30f2db04911b5514ab545964505
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-03-02 00:44:55 +0000

    deleting home app

--- code/django_webapp/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-
-def home_view(request):
-    return render(request, "home/home.html")
-
-def contact_view(request):
-    return render(request, "home/contact_page.html")
