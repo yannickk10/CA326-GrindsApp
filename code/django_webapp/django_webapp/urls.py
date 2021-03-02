a4ec880548554e3116c1820106db5450c4fa7cdd
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-03-02 12:00:18 +0000

    added logout url

--- code/django_webapp/django_webapp/urls.py
+++ code/django_webapp/django_webapp/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 from quiz.views import tutor, students
@@ -27,4 +28,5 @@ urlpatterns = [
     path('login/', user_views.login_page, name="login"),
     path('student-register/', user_views.student_register, name="student-register"),
     path('tutor-register/', user_views.tutor_register, name="tutor-register"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
