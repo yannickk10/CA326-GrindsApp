8685df7190fe74ef4bcf91dbdabb5a91eb790ef8
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-03-02 10:43:02 +0000

    tutor course model setup

--- code/django_webapp/dashboard/models.py
+++ code/django_webapp/dashboard/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.html import escape, mark_safe
+from quiz.models import User, Subject, Student
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    student = models.ManyToManyField('Student', blank=True)
+    tutor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses')
+
+    def __str__(self):
+        return self.name
