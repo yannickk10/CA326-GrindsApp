d148f171af27027b67dc9bfec01c4dbd66da99b6
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-03-02 11:44:53 +0000

    fixed typo

--- code/django_webapp/dashboard/views/welcome.py
+++ code/django_webapp/dashboard/views/welcome.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def welcome_view(request):
-    return render(request, "dasboard/welcome.html")
+    return render(request, "dashboard/welcome.html")
 
 def contact_view(request):
     return render(request, "dashboard/contact.html")
