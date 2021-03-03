d38af4d60e9f6d4b6b33aaca8d108937ef8bdaf3
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-03-03 11:38:22 +0000

    fixed namespace issue

--- code/django_webapp/dashboard/urls.py
+++ code/django_webapp/dashboard/urls.py
@@ -3,17 +3,10 @@ from .views import tutor_dashboard, student_dashboard
 
 urlpatterns = [
 
-<<<<<<< HEAD
-    path('student-dashboard/', include(([
-            path('home/', students.CourseListView.as_view(), name='course_home'),
-            path('home/<int:pk>/', students.CourseDetailView.as_view(), name='course_detail'),
-        ], 'quiz'), namespace='student-dashboard')),
-=======
     path('studentdash/', include(([
             path('home/', student_dashboard.CourseListView.as_view(), name='course_home'),
             path('home/<int:pk>/', student_dashboard.CourseDetailView.as_view(), name='course_detail'),
         ], 'quiz'), namespace='studentdash')),
->>>>>>> develop
 
     path('tutordash/', include(([
         path('home/', tutor_dashboard.home, name='home'),
