8fcdc35ee2cf3996cad4990e035715a36af96b61
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-03-09 18:07:52 +0000

    duno

--- code/django_webapp/quiz/decorators.py
+++ code/django_webapp/quiz/decorators.py
@@ -2,7 +2,7 @@ from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.decorators import user_passes_test
 
 
-def student_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+def student_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='/login/'):
     actual_decorator = user_passes_test(
         lambda u: u.is_active and u.is_student,
         login_url=login_url,
@@ -13,7 +13,7 @@ def student_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, log
     return actual_decorator
 
 
-def tutor_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+def tutor_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='/login/'):
     actual_decorator = user_passes_test(
         lambda u: u.is_active and u.is_tutor,
         login_url=login_url,
