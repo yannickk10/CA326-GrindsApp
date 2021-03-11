b98cbf4151863257f7028e04fcdc472031025465
ndjatay2 <yann.ndjatangsanbeya2@mail.dcu.ie>
2021-03-11 12:07:49 +0000

    reset database and prepared for demo

--- code/django_webapp/quiz/migrations/0002_create_initial_subjects.py
+++ code/django_webapp/quiz/migrations/0002_create_initial_subjects.py
@@ -3,11 +3,15 @@ from django.db import migrations
 
 def create_subjects(apps, schema_editor):
     Subject = apps.get_model('quiz', 'Subject')
-    Subject.objects.create(name='Arts', color='#343a40')
+    Subject.objects.create(name='Arts', color='#f44336')
     Subject.objects.create(name='Computing', color='#007bff')
-    Subject.objects.create(name='Math', color='#28a745')
+    Subject.objects.create(name='Math', color='#651fff')
     Subject.objects.create(name='Biology', color='#17a2b8')
     Subject.objects.create(name='History', color='#ffc107')
+    Subject.objects.create(name='English', color='#e65100')
+    Subject.objects.create(name='Geography', color='#28a745')
+
+
 
 
 class Migration(migrations.Migration):
